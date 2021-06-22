d85ef09a6321a0dcc9fc3ac9060e333c790ce83b
yyj7952 <yyj7952@gmail.com>
2021-06-22 10:27:36 +0900

    first commit

---
+++ src/main/java/com/example/demo/Tst1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tst1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tst1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/Tst1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tst1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
